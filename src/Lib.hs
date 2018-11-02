c70c850060c7493cc88f8df7e0ffaac76dcffea8
Kazuki Iida <elkel53930@gmail.com>
2018-11-02 15:19:01 +0900

    プロセスを表示

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,22 +6,38 @@ someFunc :: IO ()
 someFunc = do
   runProcess sample
 
-type Event = String
+data Event =
+    Event String
+  | Tau
+  | Check -- 成功終了
+  deriving (Eq)
 
 data Process =
     Prefix Event Process
   | ExtCh Process Process
   | Stop
+  deriving (Eq)
+
+instance Show Event where
+  show (Event e) = e
+  show Tau = "Tau"
+  show Check = "Check"
+
+instance Show Process where
+  show (Prefix e p) = show e ++ " -> " ++ show p
+  show (ExtCh p1 p2) = "(" ++ show p1 ++ ") [] (" ++ show p2 ++ ")"
+  show Stop = "Stop"
 
 sample :: Process
-sample = ExtCh (Prefix "A" . Prefix "B" $ Stop) (Prefix "C" $ Stop)
+sample = ExtCh (Prefix (Event "A") . Prefix (Event "B") $ Stop) (Prefix (Event "C") $ Stop)
 
-runProcess :: Process -> IO()
+runProcess :: Process -> IO ()
 runProcess Stop = putStrLn "Stop"
 runProcess p = do
+  putStrLn $ show p
   e <- getLine
-  if isAcceptableEvent p e
-    then do {putStrLn e; runProcess $ getNextProcess p e}
+  if isAcceptableEvent p (Event e)
+    then do {putStrLn e; runProcess $ getNextProcess p (Event e)}
     else do {putStrLn "Cannot accept."; runProcess $ p}
 
 isAcceptableEvent :: Process -> Event -> Bool
