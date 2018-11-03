be711a8866c37cac686b5aa119cdec53119576ce
Kazuki Iida <elkel53930@gmail.com>
2018-11-03 13:54:27 +0900

    add test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,21 @@
+import Lib
+
+import Control.Monad
+
+prExtCh = ExtCh (Prefix (Event "A") . Prefix (Event "B") $ Stop) (Prefix (Event "C") $ Stop)
+evExtCh1 = [Event "A", Event "B"]
+evExtCh2 =  [Event "C"]
+evExtCh3 =  [Event "D"]
+
+tests :: [(Process, [Event])]
+tests = [ (prExtCh, evExtCh1)
+        , (prExtCh, evExtCh2)
+        , (prExtCh, evExtCh3)
+        ]
+
+doTest :: (Process, [Event]) -> Process
+doTest (p, es) = foldl getNextProcess p es
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  forM_ tests (\t -> print $ doTest t)
